75bc12590d5e24892e6aa44bf8a365a114040eac
Oshioke Bello <47157914+oshkbello@users.noreply.github.com>
2020-10-03 18:15:47 -0700

    Update create_habit.php

--- Habit_tracker/create_habit.php
+++ Habit_tracker/create_habit.php
@@ -6,17 +6,19 @@ include('htdb_con.php');
 if (isset($_POST["submit"])) {
 
 	$habit_title = $_POST["habit_title"];
-	$user_code = $_POST["user_code"];
 
 	//verify its not empty
-	if (!empty($habit_title) && !empty($user_code)){
+	if (!empty($habit_title){
 
 		#Saving new habits into a text file
 		$habit = str_replace(' ', '_', $habit_title);
 		$fileName = "./NewHabits/".$habit.".txt";
-		$newFileContent = PHP_EOL .str_replace('_', ' ', $habit_title);
+		$newFileContent = str_replace('_', ' ', $habit_title);
 		if (file_put_contents($fileName, $newFileContent)){
 			echo "File Created(". basename($newFileContent.")");
+			//$current = file_get_contents($fileName);
+			$current = PHP_EOL."0";
+			file_put_contents($fileName, $current, FILE_APPEND);
 		}else{
 			echo "Cannot create file (".basename($newFileContent.")");
 		}
